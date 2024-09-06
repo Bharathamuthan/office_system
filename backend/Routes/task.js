37ba7147c46394789da3db0d847c5e12da2a25fc
kalakkalchandru <schandru9506@gmail.com>
2024-09-06 16:08:47 +0530

    push the code

--- backend/Routes/task.js
+++ backend/Routes/task.js
@@ -5,6 +5,6 @@ const taskController = require('../Controller/taskcontroller');
 const router = express.Router();
 
 router.post('/create',  taskController.createTask);
-router.get('/tasks',  taskController.getTasks);
+router.get('/tasks',  taskController.updateTask);
 
 module.exports = router;
